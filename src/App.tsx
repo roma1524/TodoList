ba373bd6673d40619fabac19d64769c7b57d2de5
roma1524 <kulikr.js@mail.ru>
2025-07-09 15:15:31 +0300

    added type for tasks & mapped tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
 import './App.css'
 import {Todolistitem} from "./TodolistItem.tsx";
 
-function App() {
+export type Task = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+// export type TaskList = {}
+
+export const App = () => {
+
+    const tasks1 : Task[] = [
+        { id: 1, title: 'HTML&CSS', isDone: true },
+        { id: 2, title: 'JS', isDone: true },
+        { id: 3, title: 'ReactJS', isDone: false },
+    ]
+
+    const tasks2 : Task[] = [
+        // { id: 1, title: 'Hello world', isDone: true },
+        // { id: 2, title: 'I am Happy', isDone: false },
+        // { id: 3, title: 'Yo', isDone: false },
+    ]
+
   return (
       <div className="app">
-       <Todolistitem/>
-       <Todolistitem/>
-       <Todolistitem/>
+       <Todolistitem title={'What to learn'} tasks={tasks1} date={`${new Date().getDate()}.${new Date().getMonth() + 1}.${new Date().getFullYear()}`}/>
+       <Todolistitem title={'What to read'} tasks={tasks2}/>
       </div>
   )
 }
--- src/TodolistItem.tsx
+++ src/TodolistItem.tsx
@@ -1,27 +1,35 @@
-export const Todolistitem = () => {
+import {FC} from "react";
+import {Task} from "./App.tsx";
+
+type PropsType = {
+    title: string
+    tasks: Task[]
+    date?: string
+}
+
+export const Todolistitem: FC<PropsType> = props => {
     return (
         <div>
-            <h3>What to learn</h3>
+            <h3>{props.title}</h3>
             <div>
                 <input/>
                 <button>+</button>
             </div>
             <ul>
-                <li>
-                    <input type="checkbox" checked={true}/> <span>HTML&CSS</span>
-                </li>
-                <li>
-                    <input type="checkbox" checked={true}/> <span>JS</span>
-                </li>
-                <li>
-                    <input type="checkbox" checked={false}/> <span>React</span>
-                </li>
+                {props.tasks.length === 0 ? 'Тасок нет' : props.tasks.map(el => {
+                    return (
+                        <li key={el.id}>
+                            <input type="checkbox" checked={el.isDone}/> <span>{el.title}</span>
+                        </li>
+                    )
+                })}
             </ul>
             <div>
                 <button>All</button>
                 <button>Active</button>
                 <button>Completed</button>
             </div>
+            <div>{props.date}</div>
         </div>
     )
 }
