6c56ae15b8f075732c86f433d4d1c6cd06f7ba28
roma1524 <kulikr.js@mail.ru>
2025-07-09 11:50:59 +0300

    create TodolistItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,12 @@
 import './App.css'
+import {Todolistitem} from "./TodolistItem.tsx";
 
 function App() {
   return (
       <div className="app">
-        <div>
-          <h3>What to learn</h3>
-          <div>
-            <input/>
-            <button>+</button>
-          </div>
-          <ul>
-            <li>
-              <input type="checkbox" checked={true}/> <span>HTML&CSS</span>
-            </li>
-            <li>
-              <input type="checkbox" checked={true}/> <span>JS</span>
-            </li>
-            <li>
-              <input type="checkbox" checked={false}/> <span>React</span>
-            </li>
-          </ul>
-          <div>
-            <button>All</button>
-            <button>Active</button>
-            <button>Completed</button>
-          </div>
-        </div>
+       <Todolistitem/>
+       <Todolistitem/>
+       <Todolistitem/>
       </div>
   )
 }
