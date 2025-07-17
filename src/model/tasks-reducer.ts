4e4ad2b578eb90387113679a9fdc87f179804ade
roma1524 <kulikr.js@mail.ru>
2025-07-17 23:25:22 +0300

    create tasksReducer

---
+++ src/model/tasks-reducer.ts
@@ -0,0 +1,46 @@
+import type {TasksState} from '../App'
+import {CreateTodolistAction} from "./todolists-reducer.ts";
+import {v1} from "uuid";
+
+const initialState: TasksState = {}
+
+export const tasksReducer = (state: TasksState = initialState, action: Actions): TasksState => {
+    switch (action.type) {
+        case 'ADD_TASK' : {
+            return {...state, [action.payload.todolistId]: [{id: v1(), title: action.payload.title, isDone: false}, ...state[action.payload.todolistId]]}
+        }
+        case 'DELETE_TASK': {
+            return {...state, [action.payload.todolistId]: state[action.payload.todolistId].filter(t => t.id !== action.payload.taskId)}
+        }
+        case 'CHANGE_TASK_STATUS': {
+            return {...state, [action.payload.todolistId]: state[action.payload.todolistId].map(t => t.id === action.payload.taskId ? {...t, isDone: action.payload.isDone} : t)}
+        }
+        case 'CHANGE_TASK_TITLE': {
+            return {...state, [action.payload.todolistId]: state[action.payload.todolistId].map(t => t.id === action.payload.taskId ? {...t, title: action.payload.title} : t)}
+        }
+        case "ADD_TODOLIST":
+            return { ...state, [action.payload.id]: [] };
+        default:
+            return state
+    }
+}
+
+export const addTaskAC = (todolistId: string, title: string) => {
+    return {type: 'ADD_TASK', payload: {todolistId, title }} as const;
+}
+export const deleteTaskAC = (todolistId: string, taskId: string) => {
+    return {type: 'DELETE_TASK', payload: {todolistId, taskId }} as const;
+}
+export const changeTaskStatusAC = (todolistId: string, taskId: string, isDone: boolean) => {
+    return {type: 'CHANGE_TASK_STATUS', payload: {todolistId, taskId, isDone }} as const;
+}
+export const changeTaskTitleAC = (todolistId: string, taskId: string, title: string) => {
+    return {type: 'CHANGE_TASK_TITLE', payload: {todolistId, taskId, title }} as const;
+}
+
+type addTaskAction = ReturnType<typeof addTaskAC>
+type deleteTaskAction = ReturnType<typeof deleteTaskAC>
+type changeTaskStatusAction = ReturnType<typeof changeTaskStatusAC>
+type changeTaskTitleAction = ReturnType<typeof changeTaskTitleAC>
+
+type Actions = CreateTodolistAction | addTaskAction | deleteTaskAction | changeTaskStatusAction | changeTaskTitleAction
