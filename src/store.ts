aefeeb0fc93f04c0ed5324b10a0d92f25656493c
roma1524 <kulikr.js@mail.ru>
2025-07-20 17:26:17 +0300

    Redux Toolkit / create store.ts

---
+++ src/store.ts
@@ -0,0 +1,19 @@
+import {combineReducers, configureStore} from '@reduxjs/toolkit'
+import {tasksReducer} from "./model/tasks-reducer.ts";
+import {todolistsReducer} from "./model/todolists-reducer.ts";
+
+
+const rootReducer = combineReducers({
+    tasks: tasksReducer,
+    todolists: todolistsReducer,
+})
+
+export const store = configureStore({
+    reducer: rootReducer,
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+// @ts-ignore
+window.store = store
